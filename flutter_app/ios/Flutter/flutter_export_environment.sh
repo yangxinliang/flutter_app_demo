33c2f84b69ad1bce69a41ca57af248e1cbf02b85
yangxinliang <yangxl3@newhope.cn>
2019-09-19 11:49:15 +0800

    提交： 第一次使用 sourceTree 提交

--- flutter_app/ios/Flutter/flutter_export_environment.sh
+++ flutter_app/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/yangxinliang/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yangxinliang/Desktop/flutter_app"
-export "FLUTTER_TARGET=/Users/yangxinliang/Desktop/flutter_app/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/yangxinliang/Desktop/flutter_app_demo/flutter_app"
+export "FLUTTER_TARGET=/Users/yangxinliang/Desktop/flutter_app_demo/flutter_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/yangxinliang/Documents/flutter/bin/cache/artifacts/engine/ios"

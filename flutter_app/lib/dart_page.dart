d66124695515fb4142210b6eb727e8bc47fb2954
yangxinliang <786550914@qq.com>
2019-09-21 19:37:18 +0800

    提交： 增加功能

--- flutter_app/lib/dart_page.dart
+++ flutter_app/lib/dart_page.dart
@@ -10,18 +10,32 @@ class DemoDart extends StatefulWidget {
 }
 
 class _DemoDartState extends State<DemoDart> {
+  List _dartList = ["定义变量", "基本的数据类型", "函数的使用", "类和对象", "抽象类", "枚举类型", "异步处理"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: new AppBar(
           title: Text("Dart基本知识"),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text("测试界面"),
-            Text("测试界面"),
-          ],
-        ));
+        body: ListView.separated(
+          itemCount: _dartList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(_dartList[index]),
+              onTap: () {
+                print("$index");
+              },
+            );
+          },
+          //分割器构造器
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider(
+              color: Colors.blue,
+            );
+          },
+        )
+
+    );
   }
 }
--- flutter_app/lib/widget_page.dart
+++ flutter_app/lib/widget_page.dart
@@ -6,17 +6,35 @@ class DemoWidget extends StatefulWidget {
 }
 
 class _DemoWidgetState extends State<DemoWidget> {
+
+  //定义一个装有widget的类名的数组
+  List _widgetList = ["基础组件", "布局类组件", "容器类组件", "可滚动组件", "功能性"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("组件的分类"),
       ),
-      body: Column(
-        children: <Widget>[
-          Text("widget的分类"),
-        ],
-      ),
+      body:  ListView.separated(
+        itemCount: _widgetList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(_widgetList[index]),
+            onTap: () {
+              print("$index");
+            },
+          );
+        },
+
+        //分割器构造器
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(
+            color: Colors.blue,
+          );
+        },
+      )
+
     );
   }
 }

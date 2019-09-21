2739027bee803d70903c5323aa0e43cfed97c247
yangxinliang <786550914@qq.com>
2019-09-21 18:10:51 +0800

    提交： 修改代码格式

--- flutter_app/lib/widget_page.dart
+++ flutter_app/lib/widget_page.dart
@@ -9,22 +9,14 @@ class _DemoWidgetState extends State<DemoWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text("组件的分类"),
       ),
-
       body: Column(
-
         children: <Widget>[
-
           Text("widget的分类"),
-
-
         ],
-
       ),
-
     );
   }
 }
